252b97dd4e323dae98dfca258866d3b98e2a65e0
Izukach <86355993+Izukach@users.noreply.github.com>
2023-04-17 18:08:40 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+let addTask = document.querySelector(".addTaskBtn");
+let tasks = document.getElementById("inputId");
+let taskContainer = document.querySelector(".tasksContainer");
+let timer = document.getElementById("numTask");
+let dateAndTime = document.getElementById("date");
+
+let count = 0;
+
+// dat();
+
+addTask.addEventListener("click", () => {
+  let request = prompt("Add Tasks");
+  tasks = request;
+  taskContainer.insertAdjacentHTML(
+    "beforebegin",
+    `<div class="taskDiv">
+    <input type="checkbox" id="input" name="" value="" />
+    <span id="inputId"> ${tasks}</span>
+  </div>`
+  );
+  count++;
+  timer.innerHTML = count;
+});
+
+// const dat = () => {
+//   const today = new Date();
+//   let date =
+//     today.getDate() + "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
+//   // console.log(date);
+//   // dateAndTime.innerHTML = date;
+// };
+
+const myDate = () => {
+  const today = new Date();
+  let date =
+    today.getDate() + "-" + (today.getMonth() + 1) + " -" + today.getFullYear();
+  dateAndTime.innerHTML = date;
+};
+
+myDate();
