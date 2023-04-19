8e9cd3b7022414a4c598c3d13f7390191075994a
Izukach <86355993+Izukach@users.noreply.github.com>
2023-04-19 12:07:53 -0700

    My App

--- main.js
+++ main.js
@@ -3,11 +3,18 @@ let tasks = document.getElementById("inputId");
 let taskContainer = document.querySelector(".tasksContainer");
 let timer = document.getElementById("numTask");
 let dateAndTime = document.getElementById("date");
+let delectbtn = document.getElementById("delectBtn");
 
 let count = 0;
 
 addTask.addEventListener("click", () => {
   let request = prompt("Add Tasks");
+  if (request === null || request === undefined) return;
+
+  if (request === "") {
+    count--;
+  }
+
   if (request !== "") {
     tasks = request;
     taskContainer.insertAdjacentHTML(
@@ -15,20 +22,12 @@ addTask.addEventListener("click", () => {
       `<div class="taskDiv">
     <input type="checkbox" id="input" name="" value="" />
     <span id="inputId"> ${tasks}</span>
+    <button id = "delectBtn"><i class="fa-solid fa-trash"></i></button>
   </div>`
     );
   } else {
-    taskContainer.remove();
+    taskContainer.hidden = true;
   }
-  // tasks = request;
-  // taskContainer.insertAdjacentHTML(
-  //   "beforebegin",
-  //   `<div class="taskDiv">
-  //   <input type="checkbox" id="input" name="" value="" />
-  //   <span id="inputId"> ${tasks}</span>
-  // </div>`
-  // );
-
   count++;
   timer.innerHTML = count;
 });
@@ -41,3 +40,7 @@ const myDate = () => {
 };
 
 myDate();
+
+delectbtn.addEventListener("click", () => {
+  console.log("happy");
+});
